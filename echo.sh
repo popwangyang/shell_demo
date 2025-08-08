bc6daece80d8f0f6ae8b5226f512b96a14f164f2
王岩 <wangyan@gzsle.com>
2025-08-08 10:04:55 +0800

    .

---
+++ echo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#-n: 忽略结尾的换行符
+
+echo -n "lsllls"
+
+echo "kskskskkskskk"
+
+#-e: 输出字符串中识别转义符
+
+echo -e "\tkskskskkskskk"
+
+echo  "kskskskkskskk"
+
+# 彩色打印
+# 打印彩色文本：重置=0，黑色=30，红色=31，绿色=32，黄色=33，蓝色=43，洋红=35，青色=36，白色=37
